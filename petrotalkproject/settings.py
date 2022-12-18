672b95d6185b45e17e288f0b730503f167c6e350
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-12-18 14:29:14 +0100

    mssage

--- petrotalkproject/settings.py
+++ petrotalkproject/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 
