e2901fcdb1a83d4b5a142473e80b9319400d5282
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-12-22 10:38:10 +0100

    message

--- petrotalkapp/views.py
+++ petrotalkapp/views.py
@@ -162,7 +162,7 @@ def logout_page(request):
 
 def profile_page(request, pk):
     user = User.objects.get(username=pk)
-    rooms = Room.objects.filter(author=request.user)
+    rooms = Room.objects.filter(author=user)
     context = {
         'user': user,
         'rooms': rooms
